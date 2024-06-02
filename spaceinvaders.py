4d23ba7c3e375735b4dd699c38b9e0fd1f45dd6c
Mishka Musaelyan <mishkamusaelyan@mishkas-mbp.mynetworksettings.com>
2024-06-02 11:10:30 -0400

    more rendering

--- spaceinvaders.py
+++ spaceinvaders.py
@@ -2,6 +2,7 @@ import pygame
 import os
 import time
 import random
+pygame.font.init()
 
 WIDTH, HEIGHT = 750, 750
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -22,16 +23,35 @@ BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
 YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
 
 # background
-BG = pygame.image.load(os.path.join("assets", "background-black.png"))
+BG = pygame.transform.scale(
+    pygame.image.load(os.path.join("assets", "background-black.png")),
+    (WIDTH, HEIGHT)
+)
 
 def main():
     run = True
     FPS = 60 # how fast the game gors
+    level = 1
+    lives = 5
+    main_font = pygame.font.SysFont("comicsans", 50)
+
     clock = pygame.time.Clock()
-    
+
+    def redraw_window():
+        WIN.blit(BG, (0, 0)) # draw image
+        # draw text
+        lives_label = main_font.render(f"Lives: {lives}", 1, (255, 255, 255))
+        level_label = main_font.render(f"Level: {level}", 1, (255, 255, 255))
+
+        WIN.blit(lives_label, (10, 10))
+        WIN.blit(level, (WIDTH - level_label.get_width() - 10, 10))
+
+
+        pygame.display.update() # render
 
     while run:
         clock.tick(FPS)
+        redraw_window()
 
         for event in pygame.event.get(): # pressing
             if event.type == pygame.QUIT: # x in corner
