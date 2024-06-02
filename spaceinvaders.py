fcb5bfe00094e279c4c42142f9941052d6a0280e
Mishka Musaelyan <mishkamusaelyan@mishkas-mbp.mynetworksettings.com>
2024-06-02 11:54:44 -0400

    add laser method

--- spaceinvaders.py
+++ spaceinvaders.py
@@ -28,6 +28,26 @@ BG = pygame.transform.scale(
     (WIDTH, HEIGHT)
 )
 
+class Laser:
+    def __init__(self, x, y, img):
+        self.x = x
+        self.y = y
+        self.img = img
+        self.mask = pygame.mask.from_surface(self.img)
+
+    def draw(self, window):
+        window.blit(self.img, (self.x, self.y))
+
+    def move(self, vel):
+        self.y += vel
+    
+    def off_screen(self, height):
+        return self.y < height and self.y >= 0
+    
+    def collision(self, obj):
+        return collide(self, obj)
+
+
 # abstract class, health=optional
 class Ship:
     def __init__(self, x, y, health=100):
@@ -70,6 +90,12 @@ class Enemy(Ship):
 
     def move(self, vel):
         self.y += vel
+
+# mask is important for hitting an object on its pixels, the object isn't a square
+def collide(obj1, obj2):
+    offset_x = obj2.x - obj1.x
+    offset_y = obj2.y - obj1.y
+    return obj1.mask.overlap(obj2.mask, (offset_x, offset_y)) != None
 
 def main():
     run = True
